c702a9dba26d6e58cb4525446ed78257799ea5a2
hamzasharuf <hamzasharuf20@gmail.com>
2020-10-13 12:31:51 +0800

    setup menus and toolbar

--- app/src/main/java/com/hamzasharuf/kitachat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hamzasharuf/kitachat/ui/home/HomeFragment.kt
@@ -1,8 +1,16 @@
 package com.hamzasharuf.kitachat.ui.home
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
-import com.google.android.material.tabs.TabLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.observe
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.hamzasharuf.kitachat.R
 import com.hamzasharuf.kitachat.databinding.FragmentHomeBinding
@@ -11,13 +19,32 @@ import com.hamzasharuf.kitachat.utils.adapters.HomePagerAdapter
 import com.hamzasharuf.kitachat.utils.adapters.HomeSection
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
+    val viewModel: HomeViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupPager()
+        setupObservers()
+        setupToolbar()
+    }
+
+    private fun setupObservers() {
+        viewModel.pagerState.observe(viewLifecycleOwner, {
+            changeFabIcon(it.ordinal)
+        })
+    }
+
+    private fun setupToolbar() {
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbarHome)
+        setHasOptionsMenu(true)
         toolbarHome.title = getString(R.string.app_name)
     }
 
@@ -27,7 +54,35 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
             tab.text = HomeSection.getItem(position).name
             viewPagerHome.setCurrentItem(tab.position, true)
         }.attach()
-        tabLayoutHome.tabGravity = TabLayout.GRAVITY_FILL
+
+        viewPagerHome.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                viewModel.setPagerState(position)
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.chats_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Timber.d(viewPagerHome.currentItem.toString())
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun changeFabIcon(position: Int){
+        fab.hide()
+        lifecycleScope.launch(Main){
+            delay(400)
+            when(position){
+                0 -> fab.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_message, null))
+                1 -> fab.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_camera_lens, null))
+                2 -> fab.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_call, null))
+            }
+            fab.show()
+        }
     }
 
     override fun getLayoutRes(): Int = R.layout.fragment_home
--- app/src/main/java/com/hamzasharuf/kitachat/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/hamzasharuf/kitachat/ui/home/HomeViewModel.kt
@@ -1,7 +1,18 @@
 package com.hamzasharuf.kitachat.ui.home
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.hamzasharuf.kitachat.ui.base.BaseViewModel
+import com.hamzasharuf.kitachat.utils.adapters.HomeSection
 
-class HomeViewModel : BaseViewModel() {
+class HomeViewModel @ViewModelInject constructor() : BaseViewModel() {
+
+    private val _pagerState = MutableLiveData<HomeSection>()
+    val pagerState: LiveData<HomeSection>
+        get() = _pagerState
+
+    fun setPagerState(position: Int) { _pagerState.value = HomeSection.values()[position] }
+
 
 }
